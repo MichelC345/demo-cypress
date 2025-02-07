624ffc6f76ef2c0175385947c26b28694ea9f9eb
MichelC345 <michel.caesarr@gmail.com>
2025-02-07 14:28:49 -0300

    Passo 3: Testando o cálculo do frete.

--- cypress/cypress/integration/spec2.js
+++ cypress/cypress/integration/spec2.js
@@ -7,4 +7,16 @@ describe('Teste End-to-End', () => {
         // Verifica se existe o livro desejado
         cy.get('[data-id=3]').should('contain.text', 'Design Patterns')
     })    
+    it('Teste 3: Calcula Frete', () => {    
+        // Calcula o frete
+        cy.get('[data-id=3]').within(() => {
+           cy.get('input').type('10000-000')
+           cy.contains('Calcular Frete').click().then
+           cy.wait(2000)
+        })
+        cy.get('.swal-text').contains('O frete é: R$')
+
+        // Fecha o pop-up com o preço do frete
+        cy.get('.swal-button').click()
+    })
   })
