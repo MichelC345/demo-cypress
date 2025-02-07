e81ca857913526da17a70ac89fac6557cfac7c71
MichelC345 <michel.caesarr@gmail.com>
2025-02-07 15:06:57 -0300

    Testando a compra de um livro.

--- cypress/cypress/integration/spec2.js
+++ cypress/cypress/integration/spec2.js
@@ -19,4 +19,13 @@ describe('Teste End-to-End', () => {
         // Fecha o pop-up com o preço do frete
         cy.get('.swal-button').click()
     })
+    it('Teste 4: Compra um Livro', () => {
+        cy.get('[data-id=2]').within(() => {
+            cy.get('input').type('10000-000')
+            cy.contains('Comprar').click().then
+            cy.wait(2000)
+        })
+        cy.get('.swal-text').contains('Sua compra foi realizada com sucesso')
+        cy.get('.swal-button').click()
+    })
   })
